460140815b9b68ee247ecc7b72f3535caa44864d
Cola850719 <adamcollins0609@gmail.com>
2026-08-13 04:48:20 +1000

    Remove flight search click handler
    Removed flight search functionality from app.js.

--- app.js
+++ app.js
@@ -679,16 +679,7 @@ document.getElementById("find-resort").onclick = function(){
 
 };
 
-// FIND FLIGHTS
 
-document.getElementById("find-flights").onclick = function(){
-
-    document.getElementById("flight-search-section").scrollIntoView({
-        behavior: "smooth",
-        block: "start"
-    });
-
-};
 
 // RECOMMENDATION ENGINE
 

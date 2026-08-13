6f62a15de0db21bf8836c49775128dd6fa5d3422
Cola850719 <adamcollins0609@gmail.com>
2026-08-13 22:49:35 +1000

    Update flights.js

--- api/flights.js
+++ api/flights.js
@@ -18,6 +18,15 @@ export default async function handler(req, res) {
             travel_class
         } = req.query;
 
+        const classMap = {
+    ECONOMY: "1",
+    PREMIUM_ECONOMY: "2",
+    BUSINESS: "3"
+};
+
+const classValue =
+    classMap[travel_class] || "1";
+
         if (
             !departure_id ||
             !arrival_id ||
